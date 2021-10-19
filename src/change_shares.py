6185a92154fefde2cffcee9ac0c116578053d39d
Rishith Bhowmick <brishi00@gmail.com>
2021-10-19 22:56:52 +0530

    displaying shares

--- src/change_shares.py
+++ src/change_shares.py
@@ -1,49 +1,34 @@
 import docker
- import pprint
- import time
- from docker_functions import *
+import pprint
+import time
+from docker_functions import *
 
 
- client = docker.from_env()
- all_containers = client.containers.list()
- # print(all_containers)
- # container_utilisation_dict = SortedDict()
- while True:
-     container_utilisation_dict = dict()
-     for container in all_containers:    
-         # pprint.pprint(container.attrs['HostConfig'])
-         # container.update(cpu_shares=2)
-         # client.refresh()
-         # pprint.pprint(container.attrs['HostConfig']['CpuShares'])
- 
-         # for i in container_stats:
-         #     pprint.pprint(i['cpu_stats'])
-         #     pprint.pprint(i['precpu_stats'])
-        #  container_stats = container.stats()
+client = docker.from_env()
+all_containers = client.containers.list()
 
-         container_stats = container.stats(stream=False)
-         if "jaeger" in container.name and "grafana" in container.name and "prometheus" in container.name:
-             continue    
+while True:
+    container_utilisation_dict = dict()
+    for container in all_containers:    
+        container_stats = container.stats(stream=False)
+        if "jaeger" in container.name and "grafana" in container.name and "prometheus" in container.name:
+            continue    
 
-         if 'system_cpu_usage' in  container_stats['precpu_stats'].keys():
-
+        if 'system_cpu_usage' in  container_stats['precpu_stats'].keys():
             percent = cpu_utilisation(container_stats)
             container_utilisation_dict[container] = percent             
-         else:            
-             pass
-         #     time.sleep(3)
-             #     # now The output is 0.02 and thats the answer.
-     try:
-        # valid_containers = {key:value for key,value in container_utilisation_dict.items() if key != 0}     
-        #  minimum_utilisation = container_utilisation_dict[min(container_utilisation_dict,key = container_utilisation_dict.get)]
-         
-         
-         average_utilisation = sum(container_utilisation_dict.values())/len(container_utilisation_dict)         
-         for container,utilisation in container_utilisation_dict.items():
-             # pprint.pprint(container.attrs['HostConfig']['CpuShares'])
-             new_shares = (1024/utilisation)*average_utilisation             
-             container.update(cpu_shares = new_shares)
-     # print(container_utilisation_dict)
-     except Exception as e:
-         print(str(e),e._traceback_.tb_lineno)
-     time.sleep(5)
+        else:            
+            pass
+                
+    try:    
+                
+        average_utilisation = sum(container_utilisation_dict.values())/len(container_utilisation_dict)         
+        for container,utilisation in container_utilisation_dict.items():
+            # pprint.pprint(container.attrs['HostConfig']['CpuShares'])
+            new_shares = (1024/utilisation)*average_utilisation             
+            print("Name: ",container.name,"Utilisation: ",utilisation,"shares ",new_shares)               
+            container.update(cpu_shares = new_shares)
+    # print(container_utilisation_dict)
+    except Exception as e:
+        print(str(e),e._traceback_.tb_lineno)
+    time.sleep(5)
