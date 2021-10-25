bc37093db75ca7cf48d4c84d3b0b2d2d9704c548
Rishith Bhowmick <brishi00@gmail.com>
2021-10-25 21:21:07 +0530

    Displaying partition utilisation

--- src/change_shares.py
+++ src/change_shares.py
@@ -4,10 +4,16 @@ import time
 from docker_functions import *
 from  concurrent.futures import ThreadPoolExecutor,as_completed
 from datetime import datetime
+import json 
+
+
 
 client = docker.from_env()
 all_containers = client.containers.list()
 num_containers = len(all_containers)
+with open("partitions.json","r") as f:
+    partitions = json.load(f.read())
+
 while True:
     container_utilisation_dict = dict()           
 
@@ -19,13 +25,16 @@ while True:
 
     try:    
                 
-        #average_utilisation = sum(container_utilisation_dict.values())/len(container_utilisation_dict)       
-        minimum_utilisation = container_utilisation_dict[min(container_utilisation_dict,key = container_utilisation_dict.get)]  
-        for container,utilisation in container_utilisation_dict.items():
-            # pprint.pprint(container.attrs['HostConfig']['CpuShares'])
-            new_shares = (utilisation/minimum_utilisation)*2             
-            print("Name: ",container.name,"| Utilisation: ",utilisation,"| shares ",new_shares)               
-            container.update(cpu_shares = int(new_shares))
+        #average_utilisation = sum(container_utilisation_dict.values())/len(container_utilisation_dict) 
+        
+        partition_utilisation = [sum([container_utilisation_dict[get_container_from_name(container,all_containers)] for container in partitions[partition]]) for partition in partitions.keys()]
+        print(partition_utilisation)
+        # minimum_utilisation = container_utilisation_dict[min(container_utilisation_dict,key = container_utilisation_dict.get)]  
+        # for container,utilisation in container_utilisation_dict.items():
+        #     # pprint.pprint(container.attrs['HostConfig']['CpuShares'])
+        #     new_shares = (utilisation/minimum_utilisation)*2             
+        #     print("Name: ",container.name,"| Utilisation: ",utilisation,"| shares ",new_shares)               
+        #     container.update(cpu_shares = int(new_shares))
     # print(container_utilisation_dict)
         print("*****")
     except Exception as e:
